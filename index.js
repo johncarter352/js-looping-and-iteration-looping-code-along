137cd05331ef9b3c3385360f5bcb0232107edcfc
johncarter352 <johncarter352@gmail.com>
2021-09-22 21:21:37 -0400

    commit tree 2b59da4ce08e9b2e71a33c0af2eccf5e178783f3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let cards = [];
+    for (let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+
+    return cards;
+}
+
+function countDown(countdown) {
+    let coundown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
